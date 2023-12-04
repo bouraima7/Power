29059a7ccb5c69e0fb123e408d1a1f54b8d9c396
Bouraima Diarra <84052714+bouraima7@users.noreply.github.com>
2023-12-04 05:48:47 -0500

    done

--- src/Power.java
+++ src/Power.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Power {
+public class Power {
+
+    //Computes the value of base exponent
+    //Recursive method public static int power ( int base, int exponent)
+    // Using recursion to return the result
+    //termination condition occurs when exponent is equal to 1
+
+    public static int power(int base, int exponent) {
+        // Base case: any number to the power of 1 is the number itself
+        if (exponent == 1) {
+            return base;
+        } else {
+            // Recursive case: base * base^(exponent-1)
+            return base * power(base, exponent - 1);
+        }
+    }
+
 }
--- src/PowerTest.java
+++ src/PowerTest.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class PowerTest {
+public class PowerTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("4^3 = " + Power.power(4, 3));
+        System.out.println("3^4 = " + Power.power(3, 4));
+        System.out.println("6^5 = " + Power.power(6, 5));
+        System.out.println("9^2 = " + Power.power(9, 2));
+        System.out.println("7^4 = " + Power.power(7, 4));
+    }
+
 }
